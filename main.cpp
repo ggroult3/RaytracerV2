5438b484d8271a31aeac829875a13d0e126114b2
Guillaume Groult <guillaume.groult3@orange.fr>
2021-05-09 19:11:11 +0200

    Estimation d'intégrale par la méthode de Monte-Carlo

--- main.cpp
+++ main.cpp
@@ -23,13 +23,79 @@ using namespace std;
 #include <algorithm>
 #include "Scene.h"
 
+#include <random>
+static default_random_engine engine(7697);
+static uniform_real_distribution<double> uniform(0, 1);
 
+void estimateCosIntegral(int power = 1) {
+	/* Nous cherchons a estimer l'integrale de cos^power sur l'intervalle [-pi/2;pi/2] par la methode de Monte-Carlo
+	* La methode consiste a echantillonner aleatoirement la fonction cos^10 ensuite suivant une loi de probabilite p.
+	* Ainsi on peut approximer par : Integrale(cos^power(x) dx) = 1/N Somme(cos^power(x_i) / p(x_i), i allant de 0 à N)
+	* N etant le nombre d'echantillon
+	* Dans notre cas, p suivra une loi normale d'esperance mu = 0 et d'ecart-type sigma = 0.25
+	* 
+	* Pour obtenir des echantillons x et y qui suivent une loi normale, nous faisons deux tirages u1 et u2 selon la loi uniforme U[0,1]
+	* et nous construisons les echantillons à partir de ces tirages via les formules de Box-Muller :
+	* x = sigma * sqrt(-2*log(u1)) * cos(2*pi*u2)
+	* y = sigma * sqrt(-2*log(u1)) * sin(2*pi*u2)
+	*/
+	int sampleSize = 100000; // Taille d'echantillon
+	double sigma = 0.25; // Ecart-type de la loi normale centree
+	double sum = 0; // Somme approximant l'integrale de cos^10(x)
+	for (int i = 0; i < sampleSize; i++) {
+		double u1 = uniform(engine); // echantillon suivant la loi uniforme U[0,1]
+		double u2 = uniform(engine); // echantillon suivant la loi uniforme U[0,1]
+		double xi = sigma * sqrt(-2 * log(u1)) * cos(2 * M_PI * u2); // echantillon gaussien détermine par la transformation de Box-Muller
+		double p_xi = 1 / (sigma * sqrt(2 * M_PI)) * exp(-xi * xi / (2 * sigma * sigma)); // probabilite de xi
+		if (xi > -M_PI / 2 && xi < M_PI / 2) { // Nous verifions que les echantillons sont bien dans le bon intervalle ([-pi/2;pi/2])
+			sum += pow(cos(xi), power) / p_xi / sampleSize;
+		}
+
+	}
+	cout << sum << endl;
+}
+
+void estimateCosDoubleIntegral(int power = 1) {
+	/* Nous cherchons a estimer l'integrale de cos^power(x*y) sur l'intervalle [-pi/2;pi/2] pour x et y par la methode de Monte-Carlo
+	* La methode consiste a echantillonner aleatoirement la fonction cos^power ensuite suivant une loi de probabilite p.
+	* Ainsi on peut approximer par : Integrale(cos^power(x+y) dx) = 1/N Somme(f(x_i + y_i) / p(x_i) / p(y_i), i allant de 0 à N)
+	* N etant le nombre d'echantillon
+	* Dans notre cas, p suivra une loi normale d'esperance mu = 0 et d'ecart-type sigma = 0.25
+	*
+	* Pour obtenir des echantillons x et y qui suivent une loi normale, nous faisons deux tirages u1 et u2 selon la loi uniforme U[0,1]
+	* et nous construisons les echantillons à partir de ces tirages via les formules de Box-Muller :
+	* x = sigma * sqrt(-2*log(u1)) * cos(2*pi*u2)
+	* y = sigma * sqrt(-2*log(u1)) * sin(2*pi*u2)
+	*/
+	int sampleSize = 100000; // Taille d'echantillon
+	double sigma = 0.25; // Ecart-type de la loi normale centree
+	double sum = 0; // Somme approximant l'integrale de cos^10(x)
+	for (int i = 0; i < sampleSize; i++) {
+		double u1 = uniform(engine); // echantillon suivant la loi uniforme U[0,1]
+		double u2 = uniform(engine); // echantillon suivant la loi uniforme U[0,1]
+		double xi = sigma * sqrt(-2 * log(u1)) * cos(2 * M_PI * u2); // echantillon gaussien détermine par la transformation de Box-Muller
+		double yi = sigma * sqrt(-2 * log(u1)) * sin(2 * M_PI * u2); // echantillon gaussien détermine par la transformation de Box-Muller
+		double p_xi = 1 / (sigma * sqrt(2 * M_PI)) * exp(-xi * xi / (2 * sigma * sigma)); // probabilite de xi
+		double p_yi = 1 / (sigma * sqrt(2 * M_PI)) * exp(-yi * yi / (2 * sigma * sigma)); // probabilite de xi
+		double p = p_xi * p_yi;
+		if (xi > -M_PI / 2 && xi < M_PI / 2 && yi > -M_PI / 2 && yi < M_PI / 2) {// Nous verifions que les echantillons sont bien dans le bon intervalle ([-pi/2;pi/2])
+			sum += pow(cos(xi + yi), power) / p / sampleSize;
+		}
+	}
+	cout << sum << endl;
+}
 
 int main() {
 
 	time_t beginTime, endTime; //Timecode de debut et de fin d'execution du code
 	time(&beginTime); 
 	cout << "Start !" << endl;
+
+	// Estimation d'integrale par Monte-Carlo
+	estimateCosIntegral(10); // Reponse attendue : 0.773126 / Reponse obtenue : 0.773862
+	estimateCosIntegral(2); // Reponse attendue : 1.5708 / Reponse obtenue : 1.56931
+	estimateCosDoubleIntegral(2); // Reponse attendue : 4.9348 / Reponse obtenue : 2.47889
+
 
 	int W = 512; // Largeur de l'image
 	int H = 512; // Hauteur de l'image
